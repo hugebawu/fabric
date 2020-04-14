6799babb01a379f5717f0d09848fdb7fdbe2a389
hugebawu <drbjhu@163.com>
2020-04-14 17:11:13 +0800

    add a new case (solo-network) and related chaincode(counter-cc) to the examples file

---
+++ examples/solo-network/create-systemChannel.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+configtxgen -outputBlock genesis.block \
+-profile SampleInsecureSolo \
+#-channelID SysCh #这里取这个系统通道ID非法，会如下错误，干脆不取了。
+#Error creating channelconfig bundle: initializing configtx manager failed: bad channel ID: channel ID 'SysCh' contains illegal characters               
+#panic: Error creating channelconfig bundle: initializing configtx manager failed: bad channel ID: channel ID 'SysCh' contains illegal characters 
---
+++ examples/solo-network/install-chaincode.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+peer chaincode install -n counter-cc -v 0 -l java -p ../counter-cc
